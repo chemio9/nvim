fb6820ec7c0378d4c967af8d9f6dffebac7d3a77
arch <chengruichen3@gmail.com>
2022-11-06 12:20:03 +0800

    feat(tree): add nvim-tree for file tree

---
+++ lua/plugin/tree.lua
@@ -0,0 +1,15 @@
+local plugin = {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- for file icons
+  },
+  tag = 'nightly' -- updated every week. (issue #1193)
+}
+
+function plugin.config()
+  require('nvim-tree').setup({
+    disable_netrw = true,
+    hijack_cursor = true,
+    hijack_netrw = true,
+  })
+end
