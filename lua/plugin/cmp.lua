556dbf6506718b59e41ba61117658bfd2f437698
arch <chengruichen3@gmail.com>
2022-11-06 11:02:58 +0800

    fix(cmp): highlight groups

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -27,8 +27,62 @@ function plugin:config()
   -- General configurations
   local cmp_conf = {}
 
+  local function setup_highlight()
+    -- Completion menu highlighting
+    local hls = {
+      PmenuSel = { bg = '#282C34', fg = 'NONE' },
+      Pmenu = { fg = '#C5CDD9', bg = '#22252A' },
+
+      CmpItemAbbrDeprecated = { fg = '#7E8294', bg = 'NONE', strikethrough = true },
+      CmpItemAbbrMatch = { fg = '#82AAFF', bg = 'NONE', bold = true },
+      CmpItemAbbrMatchFuzzy = { fg = '#82AAFF', bg = 'NONE', bold = true },
+      CmpItemMenu = { fg = '#C792EA', bg = 'NONE', italic = true },
+
+      CmpItemKindField = { fg = '#EED8DA', bg = '#B5585F' },
+      CmpItemKindProperty = { fg = '#EED8DA', bg = '#B5585F' },
+      CmpItemKindEvent = { fg = '#EED8DA', bg = '#B5585F' },
+
+      CmpItemKindText = { fg = '#C3E88D', bg = '#9FBD73' },
+      CmpItemKindEnum = { fg = '#C3E88D', bg = '#9FBD73' },
+      CmpItemKindKeyword = { fg = '#C3E88D', bg = '#9FBD73' },
+
+      CmpItemKindConstant = { fg = '#FFE082', bg = '#D4BB6C' },
+      CmpItemKindConstructor = { fg = '#FFE082', bg = '#D4BB6C' },
+      CmpItemKindReference = { fg = '#FFE082', bg = '#D4BB6C' },
+
+      CmpItemKindFunction = { fg = '#EADFF0', bg = '#A377BF' },
+      CmpItemKindStruct = { fg = '#EADFF0', bg = '#A377BF' },
+      CmpItemKindClass = { fg = '#EADFF0', bg = '#A377BF' },
+      CmpItemKindModule = { fg = '#EADFF0', bg = '#A377BF' },
+      CmpItemKindOperator = { fg = '#EADFF0', bg = '#A377BF' },
+
+      CmpItemKindVariable = { fg = '#C5CDD9', bg = '#7E8294' },
+      CmpItemKindFile = { fg = '#C5CDD9', bg = '#7E8294' },
+
+      CmpItemKindUnit = { fg = '#F5EBD9', bg = '#D4A959' },
+      CmpItemKindSnippet = { fg = '#F5EBD9', bg = '#D4A959' },
+      CmpItemKindFolder = { fg = '#F5EBD9', bg = '#D4A959' },
+
+      CmpItemKindMethod = { fg = '#DDE5F5', bg = '#6C8ED4' },
+      CmpItemKindValue = { fg = '#DDE5F5', bg = '#6C8ED4' },
+      CmpItemKindEnumMember = { fg = '#DDE5F5', bg = '#6C8ED4' },
+
+      CmpItemKindInterface = { fg = '#D8EEEB', bg = '#58B5A8' },
+      CmpItemKindColor = { fg = '#D8EEEB', bg = '#58B5A8' },
+      CmpItemKindTypeParameter = { fg = '#D8EEEB', bg = '#58B5A8' },
+    }
+
+    for k, v in pairs(hls) do
+      if type(v) == 'table' then
+        vim.api.nvim_set_hl(0, k, v)
+      else
+        vim.highlight.link(k, v, true)
+      end
+    end
+  end
+
   -- Define highlight groups for the window.completion.winhighlight below
-  highlight()
+  setup_highlight()
   cmp_conf.window = {
     completion = {
       winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
@@ -154,60 +208,6 @@ function plugin:config()
 
   local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
   cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-end
-
-local function highlight()
-  -- Completion menu highlighting
-  local hls = {
-    PmenuSel = { bg = '#282C34', fg = 'NONE' },
-    Pmenu = { fg = '#C5CDD9', bg = '#22252A' },
-
-    CmpItemAbbrDeprecated = { fg = '#7E8294', bg = 'NONE', strikethrough = true },
-    CmpItemAbbrMatch = { fg = '#82AAFF', bg = 'NONE', bold = true },
-    CmpItemAbbrMatchFuzzy = { fg = '#82AAFF', bg = 'NONE', bold = true },
-    CmpItemMenu = { fg = '#C792EA', bg = 'NONE', italic = true },
-
-    CmpItemKindField = { fg = '#EED8DA', bg = '#B5585F' },
-    CmpItemKindProperty = { fg = '#EED8DA', bg = '#B5585F' },
-    CmpItemKindEvent = { fg = '#EED8DA', bg = '#B5585F' },
-
-    CmpItemKindText = { fg = '#C3E88D', bg = '#9FBD73' },
-    CmpItemKindEnum = { fg = '#C3E88D', bg = '#9FBD73' },
-    CmpItemKindKeyword = { fg = '#C3E88D', bg = '#9FBD73' },
-
-    CmpItemKindConstant = { fg = '#FFE082', bg = '#D4BB6C' },
-    CmpItemKindConstructor = { fg = '#FFE082', bg = '#D4BB6C' },
-    CmpItemKindReference = { fg = '#FFE082', bg = '#D4BB6C' },
-
-    CmpItemKindFunction = { fg = '#EADFF0', bg = '#A377BF' },
-    CmpItemKindStruct = { fg = '#EADFF0', bg = '#A377BF' },
-    CmpItemKindClass = { fg = '#EADFF0', bg = '#A377BF' },
-    CmpItemKindModule = { fg = '#EADFF0', bg = '#A377BF' },
-    CmpItemKindOperator = { fg = '#EADFF0', bg = '#A377BF' },
-
-    CmpItemKindVariable = { fg = '#C5CDD9', bg = '#7E8294' },
-    CmpItemKindFile = { fg = '#C5CDD9', bg = '#7E8294' },
-
-    CmpItemKindUnit = { fg = '#F5EBD9', bg = '#D4A959' },
-    CmpItemKindSnippet = { fg = '#F5EBD9', bg = '#D4A959' },
-    CmpItemKindFolder = { fg = '#F5EBD9', bg = '#D4A959' },
-
-    CmpItemKindMethod = { fg = '#DDE5F5', bg = '#6C8ED4' },
-    CmpItemKindValue = { fg = '#DDE5F5', bg = '#6C8ED4' },
-    CmpItemKindEnumMember = { fg = '#DDE5F5', bg = '#6C8ED4' },
-
-    CmpItemKindInterface = { fg = '#D8EEEB', bg = '#58B5A8' },
-    CmpItemKindColor = { fg = '#D8EEEB', bg = '#58B5A8' },
-    CmpItemKindTypeParameter = { fg = '#D8EEEB', bg = '#58B5A8' },
-  }
-
-  for k, v in pairs(hls) do
-    if type(v) == 'table' then
-      vim.api.nvim_set_hl(0, k, v)
-    else
-      vim.highlight.link(k, v, true)
-    end
-  end
 end
 
 return plugin
