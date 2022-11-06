69d0849afc1d2648a42cb87d3d842d846f0aed7d
arch <chengruichen3@gmail.com>
2022-11-06 09:30:00 +0800

    feat(editing): luasnip support in cmp

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -26,6 +26,12 @@ function plugin:config()
   local lspkind = require 'lspkind'
   local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
 
+  local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+  end
+
+  local luasnip = require 'luasnip'
   -- General configurations
   cmp.setup {
     snippet = {
@@ -44,18 +50,27 @@ function plugin:config()
       ['<C-e>'] = cmp.mapping.abort(),
       -- Accept currently selected item. And only confirm explicitly selected items.
       ['<CR>'] = cmp.mapping.confirm { select = true },
-      -- Intellij-like mapping:
-      -- If a completion is selected, insert this one.
       ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          local entry = cmp.get_selected_entry()
-          if entry then
-            cmp.confirm()
-          end
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
         else
           fallback()
         end
-      end, { 'i', 's', 'c' }),
+      end, { 'i', 's' }),
+
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
@@ -67,7 +82,7 @@ function plugin:config()
     formatting = {
       format = lspkind.cmp_format {
         mode = 'symbol_text',
-        maxwidth = 50, 
+        maxwidth = 50,
         before = function(entry, vim_item)
           vim_item.menu = '[' .. string.upper(entry.source.name) .. ']'
           return vim_item
@@ -115,10 +130,10 @@ function plugin:config()
   -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
   --   capabilities = capabilities,
   -- }
-  
+
   cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
   require('nvim-autopairs').setup {
-    enable_check_bracket_line = false
+    enable_check_bracket_line = false,
   }
 end
 
