234a5f8fd8b1174f4768c178a91cab1a018c4dc6
arch <chengruichen3@gmail.com>
2022-11-06 13:59:11 +0800

    fix(cmp): avoid deprecated update_capatibilities

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -40,8 +40,8 @@ function plugin.config()
     end, bufopts)
   end
 
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
+  
 end
 
 return plugin
