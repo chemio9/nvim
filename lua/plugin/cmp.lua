068e385717b5b501b9fe278bded6efc195a3e97a
arch <chengruichen3@gmail.com>
2022-11-06 10:55:23 +0800

    refactor(cmp): split cmp configs into table assignment

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -23,8 +23,21 @@ local plugin = {
 function plugin:config()
   -- Set up nvim-cmp.
   local cmp = require 'cmp'
-  local lspkind = require 'lspkind'
-  local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+
+  -- General configurations
+  local cmp_conf = {}
+
+  -- Define highlight groups for the window.completion.winhighlight below
+  highlight()
+  cmp_conf.window = {
+    completion = {
+      winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
+      col_offset = -3,
+      side_padding = 0,
+    },
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  }
 
   local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -32,76 +45,120 @@ function plugin:config()
   end
 
   local luasnip = require 'luasnip'
-  -- General configurations
-  cmp.setup {
-    snippet = {
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body)
-      end,
-    },
-    window = {
-      completion = {
-        winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
-        col_offset = -3,
-        side_padding = 0,
-      },
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert {
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      -- Accept currently selected item. And only confirm explicitly selected items.
-      ['<CR>'] = cmp.mapping.confirm { select = true },
-      ['<Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
-
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
-    },
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-    }, {
-      { name = 'buffer' },
-      { name = 'path' },
-    }),
-    formatting = {
-      fields = { 'kind', 'abbr', 'menu' },
-      format = function(entry, vim_item)
-        local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
-        local strings = vim.split(kind.kind, '%s', { trimempty = true })
-        kind.kind = ' ' .. strings[1] .. ' '
-        kind.menu = '    (' .. strings[2] .. ')'
-
-        return kind
-      end,
-    },
-    view = {
-      entries = { name = 'custom', selection_order = 'near_cursor' },
-    },
+  cmp_conf.snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
   }
 
+  cmp_conf.mapping = cmp.mapping.preset.insert {
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm { select = true },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }
+
+  cmp_conf.sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+
+  local lspkind = require 'lspkind'
+  cmp_conf.formatting = {
+    fields = { 'kind', 'abbr', 'menu' },
+    format = function(entry, vim_item)
+      local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
+      local strings = vim.split(kind.kind, '%s', { trimempty = true })
+      kind.kind = ' ' .. strings[1] .. ' '
+      kind.menu = '    (' .. strings[2] .. ')'
+
+      return kind
+    end,
+  }
+
+  cmp_conf.view = {
+    entries = { name = 'custom', selection_order = 'near_cursor' },
+  }
+
+  cmp.setup(cmp_conf)
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('text', {
+    sources = {
+      { name = 'calc' },
+    },
+  })
+
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- TODO: You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    }),
+  })
+
+  -- Use buffer source for `/` and `?`
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' },
+    },
+  })
+
+  -- Use cmdline & path source for ':'
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' },
+    }, {
+      { name = 'cmdline' },
+    }),
+  })
+
+  -- TODO
+  -- Set up lspconfig.
+  -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+  --   capabilities = capabilities,
+  -- }
+
+  require('nvim-autopairs').setup {
+    enable_check_bracket_line = false,
+  }
+
+  local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+end
+
+local function highlight()
   -- Completion menu highlighting
-  local highlight_group = {
+  local hls = {
     PmenuSel = { bg = '#282C34', fg = 'NONE' },
     Pmenu = { fg = '#C5CDD9', bg = '#22252A' },
 
@@ -143,60 +200,14 @@ function plugin:config()
     CmpItemKindColor = { fg = '#D8EEEB', bg = '#58B5A8' },
     CmpItemKindTypeParameter = { fg = '#D8EEEB', bg = '#58B5A8' },
   }
-  function add_hl(hls)
-    for k, v in pairs(hls) do
-      if type(v) == 'table' then
-        vim.api.nvim_set_hl(0, k, v)
-      else
-        vim.highlight.link(k, v, true)
-      end
+
+  for k, v in pairs(hls) do
+    if type(v) == 'table' then
+      vim.api.nvim_set_hl(0, k, v)
+    else
+      vim.highlight.link(k, v, true)
     end
   end
-  add_hl(highlight_group)
-  cmp.setup.filetype('text', {
-    sources = {
-      { name = 'calc' },
-    },
-  })
-  -- Set configuration for specific filetype.
-  cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'cmp_git' }, -- TODO: You can specify the `cmp_git` source if you were installed it.
-    }, {
-      { name = 'buffer' },
-    }),
-  })
-
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' },
-    },
-  })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' },
-    }, {
-      { name = 'cmdline' },
-    }),
-  })
-
-  -- TODO
-  -- Set up lspconfig.
-  -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-  --   capabilities = capabilities,
-  -- }
-
-  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-  require('nvim-autopairs').setup {
-    enable_check_bracket_line = false,
-  }
 end
 
 return plugin
