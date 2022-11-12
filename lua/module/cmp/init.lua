23fcc298ecf6c63e9b22c37fa26ea2d8a899e784
arch <chengruichen3@gmail.com>
2022-11-12 20:37:27 +0800

    refactor(cmp): split autopairs off cmp config

--- lua/module/cmp/init.lua
+++ lua/module/cmp/init.lua
@@ -117,8 +117,6 @@ function plugin.config()
     }),
   })
 
-  local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 end
 
 return plugin
--- lua/module/pairs.lua
+++ lua/module/pairs.lua
@@ -1,11 +1,15 @@
 local plugin = {
   'windwp/nvim-autopairs',
+  after = 'hrsh7th/nvim-cmp',
 }
 
 function plugin.config()
   require('nvim-autopairs').setup {
     enable_check_bracket_line = false,
   }
+
+  local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+  require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done())
 end
 
 return plugin
