48ea825b310f1c48be4cfc09a8e23cf4f67629c5
arch <chengruichen3@gmail.com>
2022-11-13 10:44:31 +0800

    feat(startup): use impatient.nvim to improve startup time

--- init.lua
+++ init.lua
@@ -1,10 +1,15 @@
 -- TODO: update commands:
 --  $> nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-
+-- use impatient and enable profiling if it is installed
+local ok, impatient = pcall(require, 'impatient')
+if ok then
+  impatient.enable_profile()
+end
 require 'core'
 
 local plugins = require 'plugins'
 plugins.init()
+plugins.add_plugin 'profile'
 plugins.add_plugin 'tree-sitter'
 plugins.add_plugin 'statusline'
 plugins.add_plugin 'theme'
