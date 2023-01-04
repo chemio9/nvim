e8f1e2fd9e950e37905e9ca95a015866662dddb7
arch <chengruichen3@gmail.com>
2023-01-04 16:14:44 +0800

    nvim(notify): window width setting

--- lua/module/notify.lua
+++ lua/module/notify.lua
@@ -1,3 +1,11 @@
 local notify = require 'notify'
-notify.setup { max_width = 25 }
+-- TODO: set the notify float window wrap lines
+notify.setup {
+  max_width = 40,
+  minimum_width = 40,
+  -- the folling code actually doesn't work
+  -- on_open = function()
+  --   vim.opt_local.wrap = true
+  -- end,
+}
 vim.notify = notify
