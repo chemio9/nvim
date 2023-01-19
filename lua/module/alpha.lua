b8df544680c482ee7dc36bd2fcb8d406d8bb684a
chenrry666 <chengruichen3@gmail.com>
2023-01-19 23:46:35 +0800

    nvim(alpha) modify footer text

--- lua/module/alpha.lua
+++ lua/module/alpha.lua
@@ -1,4 +1,4 @@
 local config = require 'alpha.themes.dashboard'
 config.section.footer.val = { ' ', ' ', ' ',
-  'Loaded ' .. require 'lazy'.stats().count .. ' plugins ', }
+  'Installed ' .. require 'lazy'.stats().count .. ' plugins ', }
 require 'alpha'.setup(config.config)
