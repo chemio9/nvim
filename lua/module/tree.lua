7be725e7fb538ba5207bf432496e28afe557965e
chenrry666 <chengruichen3@gmail.com>
2023-01-20 20:12:45 +0800

    nvim(nvim-tree) new extended config

--- lua/module/tree.lua
+++ lua/module/tree.lua
@@ -1,4 +1,39 @@
 require 'nvim-tree'.setup {
-  hijack_cursor = true,
+  disable_netrw = true,
   hijack_netrw = true,
+  renderer = {
+    indent_markers = {
+      enable = true,
+    },
+    icons = {
+      show = {
+        git = false,
+      },
+    },
+    special_files = {
+      'Cargo.toml',
+      'Makefile',
+      'README.md',
+      'readme.md',
+      'PKGBUILD',
+      'package.json',
+      'CMakeLists.txt',
+    },
+  },
+  system_open = {
+    cmd = 'xdg-open',
+  },
+  diagnostics = {
+    enable = true,
+  },
+  git = {
+    enable = false,
+  },
+  modified = {
+    enable = true,
+  },
+  live_filter = {
+    prefix = '[FILTER]: ',
+    always_show_folders = false,
+  },
 }
