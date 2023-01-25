26ff9b732667d971d8220403db0a275a78b95962
chenrry666 <chengruichen3@gmail.com>
2023-01-25 22:30:04 +0800

    nvim(comment) fix: comment.nvim lazy-load

--- lua/plugins/cmp/comment.lua
+++ lua/plugins/cmp/comment.lua
@@ -1,7 +1,7 @@
 return {
   {
     'numToStr/Comment.nvim',
-    keys = { 'gc', 'gb' },
+    keys = { { 'gc', mode = { 'n', 'v' } }, { 'gb', mode = { 'n', 'v' } } },
     config = function()
       require 'module.cmp.comment'
     end,
