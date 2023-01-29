3c06d3447d08c8e1e3403a52f3fb76c5b2126a70
chenrry666 <chengruichen3@gmail.com>
2023-01-30 02:11:49 +0800

    nvim(cmp) change completion item style

--- lua/module/cmp/kind.lua
+++ lua/module/cmp/kind.lua
@@ -9,29 +9,28 @@ local function update(config)
   local icons = require 'nvim-web-devicons'
   config.formatting = {
     format = function(entry, vim_item)
+      local item = lspkind.cmp_format {
+        maxwidth = 60, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        preset = 'codicons',
+        mode = 'symbol_text',
+        menu = {
+          buffer = '[Buf]',
+          nvim_lsp = '[LSP]',
+          luasnip = '[Snip]',
+          nvim_lua = '[Lua]',
+          latex_symbols = '[Tex]',
+          path = '[Path]',
+        },
+      } (entry, vim_item)
       if vim.tbl_contains({ 'path' }, entry.source.name) then
         local icon, hl_group = icons.get_icon(entry:get_completion_item().label)
         if icon then
-          vim_item.kind = icon
-          vim_item.kind_hl_group = hl_group
-          vim_item.menu = '[File]'
-          return vim_item
+          item.kind = icon .. ' File'
+          item.kind_hl_group = hl_group
         end
       end
-      return lspkind.cmp_format {
-        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-        preset = 'codicons',
-        mode = 'symbol',
-        menu = {
-          buffer = '[Buffer]',
-          nvim_lsp = '[LSP]',
-          luasnip = '[LuaSnip]',
-          nvim_lua = '[Lua]',
-          latex_symbols = '[Latex]',
-          path = '[Folder]',
-        },
-      } (entry, vim_item)
+      return item
     end,
   }
 end
