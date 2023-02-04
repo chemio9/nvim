cb91317083bd714ef584145b686910e7d60ddd76
chenrry666 <chengruichen3@gmail.com>
2023-02-05 02:06:01 +0800

    nvim(cmp) completion item style changed

--- lua/module/cmp/init.lua
+++ lua/module/cmp/init.lua
@@ -1,25 +1,52 @@
--- Set up nvim-cmp.
 local cmp = require 'cmp'
+local lspkind = require 'lspkind'
+local icons = require 'nvim-web-devicons'
+local luasnip = require 'luasnip'
 
--- General configurations
 local config = {}
 
-local kind = require 'module.cmp.kind'
-kind.update(config)
+config.window = {
+  -- completion = cmp.config.window.bordered(),
+  documentation = cmp.config.window.bordered(),
+}
 
-local luasnip = require 'luasnip'
+config.formatting = {
+  format = function(entry, vim_item)
+    -- provide a completion_item in this style:
+    --    completion_text [icon] [kind] [source]
+    --  e.g.
+    --    |completion    Text [LSP]|
+    local item = lspkind.cmp_format {
+      maxwidth = 60, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      preset = 'codicons',
+      mode = 'symbol_text',
+      menu = {
+        buffer = '[Buf]',
+        nvim_lsp = '[LSP]',
+        luasnip = '[Snip]',
+        nvim_lua = '[Lua]',
+        latex_symbols = '[Tex]',
+        path = '[Path]',
+      },
+    } (entry, vim_item)
+    if vim.tbl_contains({ 'path' }, entry.source.name) then
+      local icon, hl_group = icons.get_icon(entry:get_completion_item().label)
+      if icon then
+        item.kind = icon .. ' File'
+        item.kind_hl_group = hl_group
+      end
+    end
+    return item
+  end,
+}
+
 config.snippet = {
   expand = function(args)
     luasnip.lsp_expand(args.body)
   end,
 }
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and
-      vim.api.nvim_buf_get_lines(0, line - 1, line, true)
-      [1]:sub(col, col):match '%s' == nil
-end
 config.mapping = {
   ['<Up>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
   ['<Down>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
@@ -27,8 +54,8 @@ config.mapping = {
   ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
   -- ['<C-j>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
   -- ['<C-k>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-  ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-  ['<C-y>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+  ['<C-y>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+  ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
   ['<C-e>'] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
   ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
   ['<CR>'] = cmp.mapping.confirm { select = true },
@@ -37,8 +64,6 @@ config.mapping = {
       cmp.select_next_item()
     elseif luasnip.expand_or_jumpable() then
       luasnip.expand_or_jump()
-    elseif has_words_before() then
-      cmp.complete()
     else
       fallback()
     end
@@ -59,25 +84,20 @@ config.sources = cmp.config.sources({
   { name = 'nvim_lsp', priority = 1000 },
   { name = 'luasnip', priority = 750 },
 }, {
+  { name = 'path', priority = 500 },
   { name = 'buffer', priority = 500 },
-  { name = 'path', priority = 250 },
+  { name = 'calc' },
 })
 
--- cmp_conf.view = {
---   entries = { name = 'custom', selection_order = 'near_cursor' },
--- }
+config.view = {
+  entries = { name = 'custom', selection_order = 'near_cursor' },
+}
 
 config.preselect = cmp.PreselectMode.None
-
 
 cmp.setup(config)
 
 -- Set configuration for specific filetype.
-cmp.setup.filetype('text', {
-  sources = {
-    { name = 'calc' },
-  },
-})
 
 cmp.setup.filetype('gitcommit', {
   sources = cmp.config.sources({
