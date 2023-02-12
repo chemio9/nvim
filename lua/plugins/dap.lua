e32551de13d1cfe458112251f6cc6d9288190baf
chenrry666 <chengruichen3@gmail.com>
2023-02-12 17:24:38 +0800

    nvim(format) dap config format

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,12 +7,9 @@ return {
       opts = { floating = { border = 'rounded' } },
       config = function(plugin, opts)
         local dap, dapui = require 'dap', require 'dapui'
-        dap.listeners.after.event_initialized['dapui_config'] =
-        function() dapui.open() end
-        dap.listeners.before.event_terminated['dapui_config'] =
-        function() dapui.close() end
-        dap.listeners.before.event_exited['dapui_config'] =
-        function() dapui.close() end
+        dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
+        dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
+        dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
         dapui.setup(opts)
       end,
     },
