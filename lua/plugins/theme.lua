af7ca35c598c2242df835636150927fdca3fb29b
chenrry666 <chengruichen3@gmail.com>
2023-02-12 17:26:25 +0800

    nvim(tokyonight) config

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,13 +2,33 @@ return {
   {
     'folke/tokyonight.nvim',
     priority = 100, -- load theme first
-    opts = {
-      style = 'dark',
-      sidebars = { 'qf', 'help', 'NvimTree' },
-    },
-    config = function(_, opts)
-      require 'tokyonight'.setup(opts)
-      vim.cmd.colorscheme 'tokyonight-night'
+    config = function()
+      local util = require('tokyonight.util')
+      require 'tokyonight'.setup {
+        style = 'night', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = 'day', -- The theme is used when the background is set to light
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'dark', -- style for sidebars, see below
+          floats = 'dark', -- style for floating windows
+        },
+        sidebars = { 'qf', 'help', 'NvimTree' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the Day style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard StatusLine and LuaLine.
+        dim_inactive = false, -- dims inactive windows
+        on_highlights = function(highlights, colors)
+          highlights.TablineSel = { bg = util.darken(colors.fg_gutter, 0.8) }
+        end,
+      }
+      vim.cmd.colorscheme 'tokyonight'
     end,
     lazy = false,
   },
