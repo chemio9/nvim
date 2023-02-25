ad39bc26436eacf72b310c2c7f037eb7177ce219
chenrry666 <chengruichen3@gmail.com>
2023-02-26 01:02:29 +0800

    nvim(dap): config changes

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,13 +4,12 @@ return {
   dependencies = {
     {
       'rcarriga/nvim-dap-ui',
-      opts = { floating = { border = 'rounded' } },
-      config = function(plugin, opts)
+      config = function()
         local dap, dapui = require 'dap', require 'dapui'
         dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
         dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
         dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
-        dapui.setup(opts)
+        dapui.setup { floating = { border = 'rounded' } }
       end,
     },
   },
