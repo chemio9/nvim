1b0ef2f23852005cb4eeb5036891cbfcfdb3cf26
chenrry666 <chengruichen3@gmail.com>
2023-02-26 16:00:39 +0800

    nvim(tokyonight): theme settings

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,7 +7,7 @@ return {
       require 'tokyonight'.setup {
         style = 'night', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         light_style = 'day', -- The theme is used when the background is set to light
-        transparent = false, -- Enable this to disable setting the background color
+        transparent = true, -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
         styles = {
           -- Style to be applied to different syntax groups
@@ -25,7 +25,7 @@ return {
         hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard StatusLine and LuaLine.
         dim_inactive = false, -- dims inactive windows
         on_highlights = function(highlights, colors)
-          highlights.TablineSel = { bg = util.darken(colors.fg_gutter, 0.8) }
+          highlights.TablineSel = { bg = util.darken(colors.fg_gutter, 0.3) }
         end,
       }
       vim.cmd.colorscheme 'tokyonight'
