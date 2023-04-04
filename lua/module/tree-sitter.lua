a09145a9d9c0d20dd9a1a339f780566baac4b54a
chenrry666 <chengruichen3@gmail.com>
2023-04-04 22:56:01 +0800

    chore nvim(cmp,treesitter): format

--- lua/module/tree-sitter.lua
+++ lua/module/tree-sitter.lua
@@ -12,13 +12,14 @@ require 'nvim-treesitter.configs'.setup {
     disable = function(lang, buf)
       local max_filesize = 100 * 1024 -- 100 KB
       local ok, stats = pcall(vim.loop.fs_stat,
-                              vim.api.nvim_buf_get_name(buf))
+        vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
         return true
       end
     end,
     additional_vim_regex_highlighting = false,
   },
+  incremental_selection = { enable = true },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
