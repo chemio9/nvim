a86dc27060116887e8e3eee7102fdcfb3107714e
chenrry666 <chengruichen3@gmail.com>
2023-07-18 13:23:55 +0800

    feat(nvim_lsp): check if the server can run before registering

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,7 +3,9 @@ local plugin = {
     'neovim/nvim-lspconfig',
     event = 'User File',
     config = function()
-      require 'module.lsp'.setup_diagnostics()
+      local LspConfigModule = require 'module.lsp'
+      local lspconfig = require 'lspconfig'
+      LspConfigModule.setup_diagnostics()
       local on_attach = function(client, bufnr)
         require 'lsp_signature'.on_attach({
           bind = true, -- This is mandatory, otherwise border config won't get registered.
@@ -11,12 +13,14 @@ local plugin = {
             border = 'rounded',
           },
         }, bufnr)
-        require 'module.lsp'.on_attach(client, bufnr)
+        LspConfigModule.on_attach(client, bufnr)
       end
 
-      require 'lspconfig'.util.default_config.capabilities = require 'module.lsp'.capabilities
-      for _, config in ipairs { 'clangd', 'lua_ls' } do
-        require('module.lsp.' .. config).setup(on_attach)
+      lspconfig.util.default_config.capabilities = require 'module.lsp'.capabilities
+      for _, config in ipairs { 'clangd', 'lua_ls', 'emmet_ls' } do
+        if vim.fn.executable(lspconfig[config].document_config.default_config.cmd[1]) then
+          require('module.lsp.' .. config).setup(on_attach)
+        end
       end
       for _, config in ipairs {
         -- 'nimls',
@@ -26,10 +30,12 @@ local plugin = {
         'html',
         'tsserver',
       } do
-        require 'lspconfig'[config].setup {
-          capabilities = require 'module.lsp'.capabilities,
-          on_attach = on_attach,
-        }
+        if vim.fn.executable(lspconfig[config].document_config.default_config.cmd[1]) then
+          lspconfig[config].setup {
+            capabilities = require 'module.lsp'.capabilities,
+            on_attach = on_attach,
+          }
+        end
       end
 
       vim.api.nvim_exec_autocmds('FileType', { group = 'lspconfig' })
@@ -65,6 +71,7 @@ local plugin = {
 
   {
     'j-hui/fidget.nvim',
+    tag = 'legacy',
     event = 'User LspSetup',
     opts = {
       window = {
