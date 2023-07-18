bc512ed28eb2e70fadc8434b8f6b0f8252b7d638
chenrry666 <chengruichen3@gmail.com>
2023-07-18 13:24:13 +0800

    feat(emmet): add emmet support for nvim

---
+++ lua/module/lsp/emmet_ls.lua
@@ -0,0 +1,19 @@
+local lspconfig = require 'lspconfig'
+local M = {}
+
+function M.setup(c)
+  lspconfig.emmet_ls.setup {
+    on_attach = c,
+    filetypes = { 'css', 'eruby', 'html', 'javascript', 'javascriptreact', 'less', 'sass', 'scss', 'svelte', 'pug',
+      'typescriptreact', 'vue' },
+    init_options = {
+      html = {
+        options = {
+          ['bem.enabled'] = true,
+        },
+      },
+    },
+  }
+end
+
+return M
