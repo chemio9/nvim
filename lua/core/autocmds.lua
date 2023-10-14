da84694973da9597a291fa3f17c06b78517e69a0
chenrry666 <chengruichen3@gmail.com>
2023-10-14 16:41:46 +0800

    fix(nvim): alpha now starts normally in newer version of nvim

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,12 +2,19 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local utils = require 'core.utils'
 
-autocmd('FileType', {
+autocmd('BufWinEnter', {
   desc = 'Make q close help, man, quickfix, dap floats',
   group = augroup('q_close_windows', { clear = true }),
-  pattern = { 'qf', 'help', 'man', 'dap-float' },
-  callback = function(event)
-    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true, nowait = true })
+  callback = function(args)
+    local buftype = vim.api.nvim_get_option_value('buftype', { buf = args.buf })
+    if vim.tbl_contains({ 'help', 'nofile', 'quickfix' }, buftype) and vim.fn.maparg('q', 'n') == '' then
+      vim.keymap.set('n', 'q', '<cmd>close<cr>', {
+        desc = 'Close window',
+        buffer = args.buf,
+        silent = true,
+        nowait = true,
+      })
+    end
   end,
 })
 
@@ -43,9 +50,15 @@ autocmd('VimEnter', {
   desc = 'Start Alpha when vim is opened with no arguments',
   group = group_name,
   callback = function()
-    local should_skip = false
-    ---@diagnostic disable: param-type-mismatch
-    if vim.fn.argc() > 0 or vim.fn.line2byte '$' ~= -1 or not vim.o.modifiable then
+    local should_skip
+    local lines = vim.api.nvim_buf_get_lines(0, 0, 2, false)
+    if
+        vim.fn.argc() > 0                                                                                    -- don't start when opening a file
+        or #lines > 1                                                                                        -- don't open if current buffer has more than 1 line
+        or (#lines == 1 and lines[1]:len() > 0)                                                              -- don't open the current buffer if it has anything on the first line
+        or #vim.tbl_filter(function(bufnr) return vim.bo[bufnr].buflisted end, vim.api.nvim_list_bufs()) > 1 -- don't open if any listed buffers
+        or not vim.o.modifiable                                                                              -- don't open if not modifiable
+    then
       should_skip = true
     else
       for _, arg in pairs(vim.v.argv) do
@@ -55,7 +68,9 @@ autocmd('VimEnter', {
         end
       end
     end
-    if not should_skip then require 'alpha'.start(true, require 'alpha'.default_config) end
+    if should_skip then return end
+    require 'alpha'.start(true, require 'alpha'.default_config)
+    vim.schedule(function() vim.cmd.doautocmd 'FileType' end)
   end,
 })
 
@@ -64,7 +79,7 @@ autocmd({ 'BufReadPost', 'BufNewFile' }, {
   callback = function(args)
     if not (vim.fn.expand '%' == '' or vim.api.nvim_get_option_value('buftype', { buf = args.buf }) == 'nofile') then
       utils.event 'File'
-      if utils.cmd('git -C "' .. vim.fn.expand '%:p:h' .. '" rev-parse', false) then utils.event 'GitFile' end
+      if utils.cmd({ 'git', '-C', '"' .. vim.fn.expand '%:p:h' .. '"', 'rev-parse' }, false) then utils.event 'GitFile' end
     end
   end,
 })
