1ff2a4ccbf9fe4172675904baf308da83b29cc05
chenrry666 <chengruichen3@gmail.com>
2023-10-17 19:02:17 +0800

    feat&fix(notify): notification window is 'wrap' now

--- lua/module/notify.lua
+++ lua/module/notify.lua
@@ -1,11 +1,17 @@
 local notify = require 'notify'
--- TODO: set the notify float window wrap lines
 notify.setup {
   max_width = 40,
-  minimum_width = 40,
-  -- the folling code actually doesn't work
-  -- on_open = function()
-  --   vim.opt_local.wrap = true
-  -- end,
+  background_colour = '#33334f',
+  on_open = function(win)
+    vim.api.nvim_win_set_config(win, { zindex = 175 })
+    if not package.loaded['nvim-treesitter'] then pcall(require, 'nvim-treesitter') end
+
+    local buf = vim.api.nvim_win_get_buf(win)
+    if not pcall(vim.treesitter.start, buf, 'markdown') then vim.bo[buf].syntax = 'markdown' end
+
+    vim.wo[win].conceallevel = 3
+    vim.wo[win].wrap = true
+    vim.wo[win].spell = false
+  end,
 }
 vim.notify = notify
