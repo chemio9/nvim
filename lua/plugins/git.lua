b0795fad97d195e2101704e6e989feebebe5a6c8
chenrry666 <chengruichen3@gmail.com>
2023-11-26 14:35:35 +0800

    feat(neogit)!: remove neogit (seldom use it)

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,11 +1,5 @@
 return {
   {
-    'TimUntersberger/neogit',
-    cmd = 'Neogit',
-    config = true,
-  },
-
-  {
     'lewis6991/gitsigns.nvim',
     enabled = vim.fn.executable 'git' == 1,
     ft = 'gitcommit',
