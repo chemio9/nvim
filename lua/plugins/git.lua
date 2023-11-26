3238feb091eca83b09592f8367e78662c1c279b9
chenrry666 <chengruichen3@gmail.com>
2023-11-26 14:38:23 +0800

    fix(gitsign): change icon (Nerdfont v3)

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,7 +11,7 @@ return {
         add = { text = '▎' },
         change = { text = '▎' },
         delete = { text = '▎' },
-        topdelete = { text = '契' },
+        topdelete = { text = '▎' },
         changedelete = { text = '▎' },
         untracked = { text = '▎' },
       },
