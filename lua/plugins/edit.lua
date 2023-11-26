1757a23de77d8871b6d6a348b62232fd96b2ee04
chenrry666 <chengruichen3@gmail.com>
2023-11-26 14:14:38 +0800

    feat(nvim): editing support: trim, dial.nvim, url-open

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -47,9 +47,7 @@ return {
   {
     'folke/flash.nvim',
     event = 'VeryLazy',
-    ---@type Flash.Config
     opts = {},
-    -- stylua: ignore
     keys = {
       { 's', mode = { 'n', 'x', 'o' }, function() require 'flash'.jump() end,       desc = 'Flash' },
       { 'S', mode = { 'n', 'x', 'o' }, function() require 'flash'.treesitter() end, desc = 'Flash Treesitter' },
@@ -72,4 +70,73 @@ return {
   },
 
   { 'famiu/bufdelete.nvim', cmd = { 'Bdelete', 'Bwipeout' } },
+
+  {
+    'cappyzawa/trim.nvim',
+    event = 'User File',
+    cmd = { 'Trim', 'TrimToggle' },
+    opts = {
+      ft_blocklist = {},
+      patterns = {
+        [[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
+      },
+      trim_on_write = true,
+      trim_trailing = true,
+      trim_last_line = true,
+      trim_first_line = true,
+    },
+  },
+
+  {
+    'sontungexpt/url-open',
+    event = 'VeryLazy',
+    cmd = 'URLOpenUnderCursor',
+    keys = {
+      { 'gx', '<cmd>URLOpenUnderCursor<CR>', desc = 'open url under cursor' },
+    },
+    config = function()
+      local status_ok, url_open = pcall(require, 'url-open')
+      if not status_ok then
+        return
+      end
+      url_open.setup {}
+    end,
+  },
+
+  {
+    'monaqa/dial.nvim',
+    keys = {
+      { '<C-a>',  function() require 'dial.map'.manipulate('increment', 'normal') end,  mode = 'n' },
+      { '<C-x>',  function() require 'dial.map'.manipulate('decrement', 'normal') end,  mode = 'n' },
+      { 'g<C-a>', function() require 'dial.map'.manipulate('increment', 'gnormal') end, mode = 'n' },
+      { 'g<C-x>', function() require 'dial.map'.manipulate('decrement', 'gnormal') end, mode = 'n' },
+      { '<C-a>',  function() require 'dial.map'.manipulate('increment', 'visual') end,  mode = 'v' },
+      { '<C-x>',  function() require 'dial.map'.manipulate('decrement', 'visual') end,  mode = 'v' },
+      { 'g<C-a>', function() require 'dial.map'.manipulate('increment', 'gvisual') end, mode = 'v' },
+      { 'g<C-x>', function() require 'dial.map'.manipulate('decrement', 'gvisual') end, mode = 'v' },
+    },
+    config = function()
+      local augend = require 'dial.augend'
+      require 'dial.config'.augends:register_group {
+        default = {
+          augend.constant.alias.bool,
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.date.alias['%Y/%m/%d'],
+        },
+        typescript = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.constant.new { elements = { 'let', 'const' } },
+        },
+        visual = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.date.alias['%Y/%m/%d'],
+          augend.constant.alias.alpha,
+          augend.constant.alias.Alpha,
+        },
+      }
+    end,
+  },
 }
