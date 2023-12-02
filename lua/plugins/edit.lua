9d922246f6296bbd68c37f18e541e0bdd7e8a849
chenrry666 <chengruichen3@gmail.com>
2023-12-02 18:12:25 +0800

    feat(nvim-fold): add nvim-origami

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -139,4 +139,14 @@ return {
       }
     end,
   },
+
+  {
+    'chrisgrieser/nvim-origami',
+    event = 'BufReadPost', -- later or on keypress would prevent saving folds
+    opts = {
+      keepFoldsAcrossSessions = true,
+      pauseFoldsOnSearch = true,
+      setupFoldKeymaps = true,
+    },
+  },
 }
