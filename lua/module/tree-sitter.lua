229410df218086bdf5b4664b060c47d535a68ed5
chenrry666 <chengruichen3@gmail.com>
2023-12-09 20:37:45 +0800

    fix(nvim-treesitter): invalid node type `heredoc_end` + add parsers

--- lua/module/tree-sitter.lua
+++ lua/module/tree-sitter.lua
@@ -1,5 +1,36 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'lua' },
+require('nvim-treesitter.configs').setup({
+  ensure_installed = {
+    'bash',
+    'c',
+    'comment',
+    'cpp',
+    'diff',
+    'embedded_template',
+    'git_config',
+    'git_rebase',
+    'gitattributes',
+    'gitcommit',
+    'gitignore',
+    'http',
+    'ini',
+    'javascript',
+    'json',
+    'json5',
+    'lua',
+    'luadoc',
+    'markdown',
+    'markdown_inline',
+    'regex',
+    'ron',
+    'ssh_config',
+    'toml',
+    'vim',
+    'vimdoc',
+    'yaml',
+    'yaml',
+    'yuck',
+    'zig',
+  },
   auto_install = false,
   sync_install = false,
   indent = {
@@ -12,9 +43,11 @@ require('nvim-treesitter.configs').setup {
     disable = function(lang, buf)
       local max_filesize = 100 * 1024 -- 100 KB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then return true end
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
     end,
     additional_vim_regex_highlighting = false,
   },
   incremental_selection = { enable = true },
-}
+})
