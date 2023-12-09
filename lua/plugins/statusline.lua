37524f29b65a5e00842615ed07228577ea6581c3
chenrry666 <chengruichen3@gmail.com>
2023-12-09 20:39:26 +0800

    feat(nvim-statusline): simpler statusline

---
+++ lua/plugins/statusline.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    'sontungexpt/sttusline',
+    branch = 'table_version',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    event = 'BufEnter',
+    opts = {
+      -- on_attach = function(create_update_group) end
+
+      -- the colors of statusline will be set follow the colors of the active buffer
+      -- statusline_color = "#fdff00",
+      statusline_color = 'StatusLine',
+      disabled = {
+        filetypes = {
+          -- "NvimTree",
+          -- "lazy",
+        },
+        buftypes = {
+          'terminal',
+        },
+      },
+      components = {
+        'mode',
+        'os-uname',
+        'filename',
+        'git-branch',
+        'git-diff',
+        '%=',
+        'diagnostics',
+        'lsps-formatters',
+        'indent',
+        'encoding',
+        'pos-cursor',
+        'pos-cursor-progress',
+      },
+    },
+    config = function(_, opts)
+      require('sttusline').setup(opts)
+    end,
+  }
+}
