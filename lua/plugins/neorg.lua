f8a5a6c4a86a3a37da76d2775120e80e960bfd95
chenrry666 <chengruichen3@gmail.com>
2024-01-14 13:49:59 +0800

    feat(neorg): add neorg

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    'nvim-neorg/neorg',
+    build = ':Neorg sync-parsers',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = { 'Neorg' },
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {},
+          ['core.concealer'] = {},
+          ['core.dirman'] = {
+            config = {
+              workspaces = {
+                notes = '~/notes',
+              },
+              default_workspace = 'notes',
+            },
+          },
+          ['core.completion'] = {
+            config = { engine = 'nvim-cmp' },
+          },
+          ['core.presenter'] = {
+            config = {
+              zen_mode = 'zen-mode',
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
+}
