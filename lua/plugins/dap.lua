232de655ce9c026b60f42de7f0b93faea1b3e78c
chenrry666 <chengruichen3@gmail.com>
2024-01-25 18:03:31 +0800

    refractor(dap)!: remove F keys, use leader please

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -25,62 +25,6 @@ return {
   end,
   keys = {
     {
-      '<F5>',
-      function()
-        require('dap').continue()
-      end,
-      desc = 'Debugger: Start',
-    },
-    {
-      '<F17>',
-      function()
-        require('dap').terminate()
-      end,
-      desc = 'Debugger: Stop',
-    }, -- Shift+F5
-    {
-      '<F29>',
-      function()
-        require('dap').restart_frame()
-      end,
-      desc = 'Debugger: Restart',
-    }, -- Control+F5
-    {
-      '<F6>',
-      function()
-        require('dap').pause()
-      end,
-      desc = 'Debugger: Pause',
-    },
-    {
-      '<F9>',
-      function()
-        require('dap').toggle_breakpoint()
-      end,
-      desc = 'Debugger: Toggle Breakpoint',
-    },
-    {
-      '<F10>',
-      function()
-        require('dap').step_over()
-      end,
-      desc = 'Debugger: Step Over',
-    },
-    {
-      '<F11>',
-      function()
-        require('dap').step_into()
-      end,
-      desc = 'Debugger: Step Into',
-    },
-    {
-      '<F23>',
-      function()
-        require('dap').step_out()
-      end,
-      desc = 'Debugger: Step Out',
-    }, -- Shift+F11
-    {
       '<leader>db',
       function()
         require('dap').toggle_breakpoint()
