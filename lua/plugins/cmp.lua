97285c54ff1746a1ca3935cd8af3560a8251b5b4
chenrry666 <chengruichen3@gmail.com>
2024-01-31 11:08:06 +0800

    feat(nvim): enable cmp-buffer plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,7 +6,7 @@ local plugin = {
     dependencies = {
       -- Completion sources
       'hrsh7th/cmp-nvim-lsp',
-      { 'hrsh7th/cmp-buffer', enabled = false },
+      'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
       'hrsh7th/cmp-calc',
