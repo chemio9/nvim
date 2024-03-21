529d252f02ed19879ee3ba9d9e6c847c6a1a054a
Clarence <chengruichen3@gmail.com>
2024-03-22 00:10:09 +0800

    feat(edit): add substitute.nvim

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -6,6 +6,24 @@ return {
   },
 
   {
+    'gbprod/substitute.nvim',
+    config = function()
+      require('substitute').setup()
+    end,
+    keys = {
+      { 's',   function() require('substitute').operator() end,          mode = 'n', { noremap = true } },
+      { 'ss',  function() require('substitute').line() end,              mode = 'n', { noremap = true } },
+      { 'S',   function() require('substitute').eol() end,               mode = 'n', { noremap = true } },
+      { 's',   function() require('substitute').visual() end,            mode = 'x', { noremap = true } },
+
+      { 'sx',  function() require('substitute.exchange').operator() end, mode = 'n', { noremap = true } },
+      { 'sxx', function() require('substitute.exchange').line() end,     mode = 'n', { noremap = true } },
+      { 'X',   function() require('substitute.exchange').visual() end,   mode = 'x', { noremap = true } },
+      { 'sxc', function() require('substitute.exchange').cancel() end,   mode = 'n', { noremap = true } },
+    },
+  },
+
+  {
     'max397574/better-escape.nvim',
     event = 'InsertCharPre',
     config = true,
