d64360a7e0d085848c544e066fa1c6b96b13f818
Clarence <chengruichen3@gmail.com>
2024-03-22 00:10:42 +0800

    feat(keymap): use m and M to 'flash' through nodes

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -68,7 +68,7 @@ return {
     opts = {},
     keys = {
       {
-        's',
+        'm', -- *m*ove
         mode = { 'n', 'x', 'o' },
         function()
           require('flash').jump()
@@ -76,28 +76,12 @@ return {
         desc = 'Flash',
       },
       {
-        'S',
+        'M',
         mode = { 'n', 'x', 'o' },
         function()
           require('flash').treesitter()
         end,
         desc = 'Flash Treesitter',
-      },
-      {
-        'r',
-        mode = 'o',
-        function()
-          require('flash').remote()
-        end,
-        desc = 'Remote Flash',
-      },
-      {
-        'R',
-        mode = { 'o', 'x' },
-        function()
-          require('flash').treesitter_search()
-        end,
-        desc = 'Treesitter Search',
       },
       {
         '<c-s>',
