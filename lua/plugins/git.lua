75923e5bbf1872612f9f7a9afbc7d3dd44c22d32
Clarence <chengruichen3@gmail.com>
2024-03-22 00:08:24 +0800

    fix(autocmd): remove GitFile

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,7 +4,7 @@ return {
     'lewis6991/gitsigns.nvim',
     enabled = vim.fn.executable 'git' == 1,
     ft = 'gitcommit',
-    event = 'User GitFile',
+    event = 'User File',
     cmd = 'Gitsigns',
     opts = {
       trouble = true,
