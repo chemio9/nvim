ed4026edff9e650637a39885bf80ce1355f2f4dc
Clarence <chengruichen3@gmail.com>
2024-04-05 14:57:46 +0800

    feat(plenary): profile key binding

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,7 +23,32 @@ return {
     config = true,
   },
 
-  'nvim-lua/plenary.nvim',
+  {
+    'nvim-lua/plenary.nvim',
+    keys = {
+      {
+        '<leader>hpb',
+        function()
+          require('plenary.profile').start('profile.log', { flame = true })
+        end,
+        desc = 'Begin profiling',
+      },
+      {
+        '<leader>hpe',
+        function()
+          require('plenary.profile').stop()
+        end,
+        desc = 'End profiling',
+      },
+    },
+    config = function()
+      require('which-key').register({
+        ['hp'] = {
+          name = '+profile',
+        },
+      }, { prefix = '<leader>' })
+    end,
+  },
 
   {
     'sindrets/diffview.nvim',
