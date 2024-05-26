2f6d2f99f72ee5f90aa79efb1b1cc8cba2150c6a
Clarence <chengruichen3@gmail.com>
2024-05-26 14:27:28 +0800

    feat(trouble): auto open trouble when open quickfix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -148,6 +148,22 @@ local plugin = {
       'TroubleToggle',
       'TroubleClose',
     },
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'qf',
+        callback = function(args)
+          local bufnr = args.buf
+          vim.defer_fn(function()
+            local winid = vim.fn.bufwinid(bufnr)
+            if winid == -1 then
+              return
+            end
+            vim.api.nvim_win_close(winid, true)
+            require('trouble').open('quickfix')
+          end, 0)
+        end,
+      })
+    end,
     opts = {
       use_diagnostic_signs = true,
     },
@@ -190,4 +206,3 @@ local plugin = {
   },
 }
 return plugin
--- vim: fdm=marker:foldlevel=0
