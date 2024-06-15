c5bbe9a1cb48bbefbae565c3846e70dc36ce3e0f
Clarence <chengruichen3@gmail.com>
2024-06-16 01:48:09 +0800

    refactor(telescope): disable telescope mapping for now

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -28,39 +28,6 @@ map('n', '<leader>gB', { function() require('telescope.builtin').git_branches() 
 map('n', '<leader>gC', { function() require('telescope.builtin').git_commits() end, desc = 'Git commits' })
 map('n', '<leader>gs', { function() require('telescope.builtin').git_status() end, desc = 'Git status' })
 
-map('n', '<leader>f', { desc = '󰍉 Find' })
-map('n', '<leader>f<CR>', { function() require('telescope.builtin').resume() end, desc = 'Resume previous search' })
-map('n', "<leader>f'", { function() require('telescope.builtin').marks() end, desc = 'Find marks' })
-map('n', '<leader>fb', { function() require('telescope.builtin').buffers() end, desc = 'Find buffers' })
-map('n', '<leader>fc',
-  { function() require('telescope.builtin').grep_string() end, desc = 'Find for word under cursor' })
-map('n', '<leader>fC', { function() require('telescope.builtin').commands() end, desc = 'Find commands' })
-map('n', '<leader>ff', { function() require('telescope.builtin').find_files() end, desc = 'Find files' })
-map('n', '<leader>fF',
-  { function() require('telescope.builtin').find_files { hidden = true, no_ignore = true } end, desc = 'Find all files' })
-map('n', '<leader>fh', { function() require('telescope.builtin').help_tags() end, desc = 'Find help' })
-map('n', '<leader>fk', { function() require('telescope.builtin').keymaps() end, desc = 'Find keymaps' })
-map('n', '<leader>fm', { function() require('telescope.builtin').man_pages() end, desc = 'Find man' })
-if has 'nvim-notify' then
-  map('n', '<leader>fn',
-    { function() require('telescope').extensions.notify.notify() end, desc = 'Find notifications' })
-end
-map('n', '<leader>fo', { function() require('telescope.builtin').oldfiles() end, desc = 'Find history' })
-map('n', '<leader>fr', { function() require('telescope.builtin').registers() end, desc = 'Find registers' })
-map('n', '<leader>ft',
-  { function() require('telescope.builtin').colorscheme { enable_preview = true } end, desc = 'Find themes' })
-map('n', '<leader>fw', { function() require('telescope.builtin').live_grep() end, desc = 'Find words' })
-map('n', '<leader>fW',
-  {
-    function()
-      require('telescope.builtin').live_grep { additional_args = function(args)
-        return vim.list_extend(args,
-          { '--hidden', '--no-ignore' })
-      end }
-    end,
-    desc = 'Find words in all files',
-  })
-
 map('n', 'K', { function()
   local winid = require('ufo').peekFoldedLinesUnderCursor()
   if not winid then
