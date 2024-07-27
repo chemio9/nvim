6995f35a860c57c5ad1e0061072f059b1d04f833
Clarence <chengruichen3@gmail.com>
2024-07-27 17:39:08 +0800

    feat(luasnip): use locally jump

--- lua/module/cmp/init.lua
+++ lua/module/cmp/init.lua
@@ -60,7 +60,7 @@ config.mapping = {
   ['<Tab>'] = cmp.mapping(function(fallback)
     if cmp.visible() then
       cmp.select_next_item()
-    elseif luasnip.expand_or_jumpable() then
+    elseif luasnip.expand_or_locally_jumpable() then
       luasnip.expand_or_jump()
     else
       fallback()
