b38fc282d98eefcd0601e6c8a29cb64ba29662a4
Clarence <chengruichen3@gmail.com>
2024-07-29 02:26:57 +0800

    fix(treesitter): add predicates and rtp when init
      Fixed: invalid node type 'delimiter'

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -3,22 +3,19 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     event = 'User File',
-    cmd = {
-      'TSBufDisable',
-      'TSBufEnable',
-      'TSBufToggle',
-      'TSDisable',
-      'TSEnable',
-      'TSToggle',
-      'TSInstall',
-      'TSInstallInfo',
-      'TSInstallSync',
-      'TSModuleInfo',
-      'TSUninstall',
-      'TSUpdate',
-      'TSUpdateSync',
-    },
-    build = ':TSUpdate',
+    build = function()
+      require('nvim-treesitter.install').update({ with_sync = true })()
+    end,
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require('lazy.core.loader').add_to_rtp(plugin)
+      require('nvim-treesitter.query_predicates')
+    end,
+    cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
     config = function()
       require 'module.tree-sitter'
     end,
