3103519a1003dd6593dc99173e747592d6c7508a
Clarence <chengruichen3@gmail.com>
2024-10-04 14:13:12 +0800

    refactor: use vim.g.colroscheme to determine colorscheme, reserve for multiple colorschemes

--- init.lua
+++ init.lua
@@ -1,9 +1,8 @@
 if vim.loader then vim.loader.enable() end -- enable vim.loader early if available
 
+vim.g.colorscheme = "nightfox"
 require 'core.options'
 require 'core.autocmds'
-
 require 'core.package'
 
 require 'core.keymap'
-vim.cmd.colorscheme 'nightfox'
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,6 +3,7 @@ return {
   {
     'EdenEast/nightfox.nvim',
     priority = 1000,
+    cond = vim.g.colorscheme == "nightfox",
     opts = {
       options = {
         styles = {             -- Style to be applied to different syntax groups
@@ -17,7 +18,13 @@ return {
           strings = 'NONE',
           variables = 'NONE',
         },
+        transparent = false,
+        dim_inactive = true,
       },
     },
+    config = function (_, opts)
+      require("nightfox").setup(opts)
+      vim.cmd.colorscheme("nightfox")
+    end
   },
 }
