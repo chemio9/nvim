a04d464a442934593b176490a0a2479c858ce05a
Clarence <chengruichen3@gmail.com>
2025-07-11 17:38:03 +0800

    fix: load treesitter immediately

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -2,18 +2,8 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    event = 'User File',
     branch = 'main',
     build = ':TSUpdate',
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require('lazy.core.loader').add_to_rtp(plugin)
-      require('nvim-treesitter.query_predicates')
-    end,
     cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
     config = function()
       require 'module.tree-sitter'
