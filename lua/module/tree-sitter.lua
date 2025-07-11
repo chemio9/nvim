eebb14eb8992d776528412a7a99a72b9997c02bb
Clarence <chengruichen3@gmail.com>
2025-07-11 17:22:45 +0800

    feat: update to nvim-treesitter main branch

--- lua/module/tree-sitter.lua
+++ lua/module/tree-sitter.lua
@@ -1,54 +1,62 @@
----@diagnostic disable: missing-fields
-require('nvim-treesitter.configs').setup({
-  ensure_installed = {
-    'bash',
-    'c',
-    'comment',
-    'cpp',
-    'diff',
-    'embedded_template',
-    'git_config',
-    'git_rebase',
-    'gitattributes',
-    'gitcommit',
-    'gitignore',
-    'http',
-    'ini',
-    'javascript',
-    'json',
-    'json5',
-    'lua',
-    'luadoc',
-    'markdown',
-    'markdown_inline',
-    'regex',
-    'ron',
-    'ssh_config',
-    'toml',
-    'vim',
-    'vimdoc',
-    'yaml',
-    'yaml',
-    'yuck',
-    'zig',
-  },
-  auto_install = false,
-  sync_install = false,
-  indent = {
-    enable = true,
-  },
-  highlight = {
-    enable = true,
-    -- disable slow treesitter highlight for large files
-    ---@diagnostic disable-next-line: unused-local
-    disable = function(lang, buf)
-      local max_filesize = 100 * 1024 -- 100 KB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
-    additional_vim_regex_highlighting = false,
-  },
-  incremental_selection = { enable = true },
+local ensure_installed = {
+  'bash',
+  'c',
+  'comment',
+  'cpp',
+  'diff',
+  'embedded_template',
+  'git_config',
+  'git_rebase',
+  'gitattributes',
+  'gitcommit',
+  'gitignore',
+  'http',
+  'ini',
+  'javascript',
+  'json',
+  'json5',
+  'lua',
+  'luadoc',
+  'markdown',
+  'markdown_inline',
+  'regex',
+  'ron',
+  'ssh_config',
+  'toml',
+  'vim',
+  'vimdoc',
+  'yaml',
+  'yaml',
+  'yuck',
+  'zig',
+}
+
+local disable = function(lang, buf)
+  local max_filesize = 100 * 1024 -- 100 KB
+  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+  if ok and stats and stats.size > max_filesize then
+    return true
+  end
+end
+
+require('nvim-treesitter').install(ensure_installed)
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('treesitter.setup', {}),
+  callback = function(args)
+    local buf = args.buf
+    local filetype = args.match
+
+    -- need some mechanism to avoid running on buffers that do not
+    -- correspond to a language (like oil.nvim buffers), this implementation
+    -- checks if a parser exists for the current language
+    local language = vim.treesitter.language.get_lang(filetype) or filetype
+    if not vim.treesitter.language.add(language) then
+      return
+    end
+
+    if not disable(language, buf) then
+      vim.treesitter.start(buf, language)
+    end
+  end,
 })
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -3,9 +3,8 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     event = 'User File',
-    build = function()
-      require('nvim-treesitter.install').update({ with_sync = true })()
-    end,
+    branch = 'main',
+    build = ':TSUpdate',
     init = function(plugin)
       -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
       -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
