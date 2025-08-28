5d598534358e793d020e2172edb590a5e46addde
Clarence <chengruichen3@gmail.com>
2025-08-28 23:21:43 +0800

    feat: fzf preset telescope

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -6,13 +6,15 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       -- calling `setup` is optional for customization
-      require('fzf-lua').setup({})
+      require('fzf-lua').setup({
+        'telescope',
+      })
     end,
     cmd = 'FzfLua',
     keys = {
       { '<leader>fg',       function() require('fzf-lua').live_grep() end,     desc = 'Live Grep (Rg)' },
       { '<leader><leader>', function() require('fzf-lua').resume() end,        desc = 'Resume Last' },
-      { '<C-x><C-f>',       function() require('fzf-lua').complete_path() end, desc = 'Complete path',  mode = 'i' },
+      { '<C-x><C-f>',       function() require('fzf-lua').complete_path() end, desc = 'Complete path', mode = 'i' },
       { '<leader>fb',       function() require('fzf-lua').buffers() end,       desc = 'buffers' },
     },
   },
