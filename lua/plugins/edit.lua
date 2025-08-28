678a9e5e962618f1158c058f659d9892e69876d8
Clarence <chengruichen3@gmail.com>
2025-08-28 23:19:15 +0800

    feat(folds): ufo fallback

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -275,12 +275,32 @@ return {
     event = 'BufReadPost',
     config = function()
       ---@diagnostic disable-next-line: missing-fields
+      ---@param bufnr number
+      ---@return Promise
+      local function customizeSelector(bufnr)
+        local function handleFallbackException(err, providerName)
+          if type(err) == 'string' and err:match 'UfoFallbackException' then
+            return require('ufo').getFolds(bufnr, providerName)
+          else
+            return require('promise').reject(err)
+          end
+        end
+        return require('ufo')
+            .getFolds(bufnr, 'lsp')
+            :catch(function(err)
+              return handleFallbackException(err, 'treesitter')
+            end)
+            :catch(function(err)
+              return handleFallbackException(err, 'indent')
+            end)
+      end
+
       require('ufo').setup({
         close_fold_kinds_for_ft = {
           default = { 'imports' },
         },
         provider_selector = function(bufnr, filetype, buftype)
-          return { 'treesitter', 'indent' }
+          return customizeSelector
         end,
       })
     end,
